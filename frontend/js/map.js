42183cb6391f11fda717186122951894d48ab749
yelis <110356354+yelis-alt@users.noreply.github.com>
2022-10-20 10:47:02 +0300

    upload

---
+++ frontend/js/map.js
@@ -0,0 +1,64 @@
+ymaps.ready(init);
+
+function init() {
+        myMap = new ymaps.Map('map', {
+            center: [55.75370903771494, 37.61981338262558],
+            zoom: 10,
+            controls: []
+        }),
+    // Создадим панель маршрутизации.
+        routePanelControl = new ymaps.control.RoutePanel({
+            options: {
+                maxWidth: '350px',
+                showHeader: true,
+                title: 'Ваш маршрут'
+            }
+        }),
+        zoomControl = new ymaps.control.ZoomControl({
+            options: {
+                size: 'small',
+                float: 'none',
+                position: {
+                    bottom: 145,
+                    right: 10
+                }
+            }
+        });
+    // Пользователь сможет построить только автомобильный маршрут.
+    routePanelControl.routePanel.options.set({
+        types: {auto: true}
+    });
+
+    myMap.controls.add(routePanelControl).add(zoomControl);
+
+    // Получим ссылку на маршрут.
+    routePanelControl.routePanel.getRouteAsync().then(function (route) {
+
+        // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
+        route.model.setParams({results: 1}, true);
+
+        // Повесим обработчик на событие построения маршрута.
+        route.model.events.add('requestsuccess', function () {
+
+            var activeRoute = route.getActiveRoute();
+            if (activeRoute) {
+                // Получим протяженность маршрута.
+                var length = route.getActiveRoute().properties.get("distance"),
+                // Вычислим стоимость доставки.
+                    price = calculate(Math.round(length.value / 1000)),
+                // Создадим макет содержимого балуна маршрута.
+                    balloonContentLayout = ymaps.templateLayoutFactory.createClass(
+                        '<span>Расстояние: ' + length.text + '</span>');
+                // Зададим этот макет для содержимого балуна.
+                route.options.set('routeBalloonContentLayout', balloonContentLayout);
+                // Откроем балун.
+                activeRoute.balloon.open();
+            }
+        });
+
+    });
+    // Функция, вычисляющая стоимость доставки.
+    function calculate(routeLength) {
+        return routeLength;
+    }
+}
