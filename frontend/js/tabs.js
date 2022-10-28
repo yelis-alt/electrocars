e9ddb5f9e9a566d0f9037890ecca3128fcd4c180
yelis <110356354+yelis-alt@users.noreply.github.com>
2022-10-28 14:06:32 +0300

    28.10

--- frontend/js/tabs.js
+++ frontend/js/tabs.js
@@ -1,28 +1,47 @@
 function openPage(pageName, elmnt, color) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
   }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-  document.getElementById(pageName).style.display = "block";
-}
+  
+  document.getElementById("defaultOpen").click();
 
-document.getElementById("defaultOpen").click();
-
-var tabs = document.getElementsByClassName('tablink');
-
-Array.prototype.forEach.call(tabs, function(tab) {
-  tab.addEventListener('click', setActiveClass);
+$("#defaultOpen").click(function(){
+    $("#defaultOpen").css({
+        'border-bottom-color': '#005999',
+        'border-bottom-width': '3.5px',
+        'border-bottom-style': 'solid',
+    });
+    $("#secondOpen").css({
+        'border-bottom-color': '#ffffff',
+    });
 });
 
-function setActiveClass(evt) {
-  Array.prototype.forEach.call(tabs, function(tab) {
-    tab.classList.remove('active');
-  });
-  
-  evt.currentTarget.classList.add('active');
-}
+$("#secondOpen").click(function(){
+    $("#secondOpen").css({
+        'border-bottom-color': '#005999',
+        'border-bottom-width': '3.5px',
+        'border-bottom-style': 'solid',
+    });
+    $("#defaultOpen").css({
+        'border-bottom-color': '#ffffff',
+    });
+});
+
+$(function(){
+    var requiredCheckboxes = $('.type_electricity');
+    requiredCheckboxes.change(function(){
+        if(requiredCheckboxes.is(':checked')) {
+        } else {
+            $('#ac').prop('checked', true);
+            $('#dc').prop('checked', true);
+        }
+    });
+});
